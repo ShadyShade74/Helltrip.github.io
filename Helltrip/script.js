aacb467b25acf599656cd30584b097a956bebcd5
ShadyShade74 <shadyshade74@gmail.com>
2025-11-25 21:47:30 +0100

    .

--- Helltrip/script.js
+++ Helltrip/script.js
@@ -1,47 +1,51 @@
-const waypoints = [{ x: 400, y: 400 }];
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
 
-canvas.width = 1280
-canvas.height = 768
-c.fillStyle = 'white'
-c.fillRect(0, 0, canvas.width, canvas.height,)
-const image = new Image()
-image.onload = () => {
-	animate()
-}
-image.src = 'media/map.png'
+canvas.width = 1280;
+canvas.height = 768;
+c.fillStyle = 'white';
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+const image = new Image();
+image.src = 'media/map.png';
+
+const waypoints = [
+    { x: 400, y: 400 }
+];
 
 class Enemy {
-	constructor({ position }) {
-		this.position = position
-		this.width = 100
-		this.height = 100
-	}
+    constructor({ position = { x: 0, y: 0 } }) {
+        this.position = position;
+        this.width = 100;
+        this.height = 100;
+    }
 
-	draw() {
-		c.fillStyle = 'red'
-		c.fillRect(this.position.x, this.position.y, this.width, this.height)
-	}
-	update() {
-		this.draw()
+    draw() {
+        c.fillStyle = 'red';
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
 
-		const waypoint = waypoints[0]
-		const xDistance = waypoint.x - this.position.x
-		const yDistance = waypoint.y - this.position.y
-		const angle = Math.atan2(yDistance, xDistance)
-		this.position.x += Math.cos(angle)
-		this.position.y += Math.sin(angle)
-	}
+    update() {
+        this.draw();
+        const waypoint = waypoints[0];
+        const yDistance = waypoint.y - this.position.y;
+        const xDistance = waypoint.x - this.position.x;
+        const angle = Math.atan2(yDistance, xDistance);
+        this.position.x += Math.cos(angle);
+        this.position.y += Math.sin(angle);
+    }
 }
 
-const enemy = new Enemy({ position: { x: 200, y: 400 } })
-const enemy2 = new Enemy({ position: { x: 0, y: 400 } })
+const enemy = new Enemy({ position: { x: 200, y: 400 } });
+const enemy2 = new Enemy({ position: { x: 0, y: 400 } });
 
 function animate() {
-	window.requestAnimationFrame(animate)
-
-	c.drawImage(image, 0, 0)
-	enemy.update()
+    window.requestAnimationFrame(animate);
+    c.drawImage(image, 0, 0);
+    enemy.update();
 }
+
+image.onload = () => {
+    animate();
+};
 
