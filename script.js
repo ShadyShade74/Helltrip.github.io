d9610cee46d2d3036a3b65d66effb62d14bc4a59
ShadyShade74 <shadyshade74@gmail.com>
2025-11-25 16:47:18 +0100

    Changes to js and img

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ const canvas = document.querySelector('canvas')
 	canvas.height = 768
 	c.fillStyle = 'white'
 	c.fillRect(0, 0, canvas.width, canvas.height,)
+    const image = new Image()
+    
+    image.onload = () => {
+        c.drawImage(image, 0, 0)
+    }
+image.src = 'media/map.png'
