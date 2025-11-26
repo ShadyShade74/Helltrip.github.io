f47e7dbfd2ed98923e73f7be090a6664d58c7c06
ShadyShade74 <shadyshade74@gmail.com>
2025-11-26 21:47:42 +0100

    class changes

--- Helltrip/js/script.js
+++ Helltrip/js/script.js
@@ -42,44 +42,7 @@ placementTilesData2d.forEach((row, y) => {
 }
 )
 
-class Enemy {
-    constructor({ position = { x: 0, y: 0 } }) {
-        this.position = position;
-        this.width = 100;
-        this.height = 100;
-        this.waypointIndex = 0 ;
-        this.center = {
-            x: this.position.x + this.width / 2,
-            y: this.position.y + this.height / 2
-        }
-    }
 
-    draw() {
-        c.fillStyle = 'red';
-        c.fillRect(this.position.x, this.position.y, this.width, this.height);
-    }
-
-    update() {
-        this.draw();
-        const waypoint = waypoints[this.waypointIndex];
-        const yDistance = waypoint.y - this.center.y;
-        const xDistance = waypoint.x - this.center.x;
-        const angle = Math.atan2(yDistance, xDistance);
-        this.position.x += Math.cos(angle);
-        this.position.y += Math.sin(angle);
-        this.center = {
-            x: this.position.x + this.width / 2,
-            y: this.position.y + this.height / 2
-        }
-
-        if (
-            Math.round(this.center.x) === Math.round(waypoint.x )&&
-            Math.round(this.center.y) === Math.round(waypoint.y) && this.waypointIndex < waypoints.length
-        ) {
-            this.waypointIndex++;
-        }
-    }
-}
 
 
 const enemies = []
